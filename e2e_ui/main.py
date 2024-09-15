2d870e24a99f2305904955ea979f1125a30b103b
hanterzoone007 <dinkilaker3@gmail.com>
2024-09-15 12:15:39 +1000

    Code optimization

--- e2e_ui/main.py
+++ e2e_ui/main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import os
 from dotenv import load_dotenv
 from selenium.webdriver.common.timeouts import Timeouts
+import pytest
 
 browser = Firefox()
 
@@ -16,44 +17,36 @@ def test_site():
     browser.get('https://www.saucedemo.com/')
     assert(browser.current_url)
 
-def test_auth():
+@pytest.fixture
+def browser_auth():
     browser.get('http://www.saucedemo.com/')
     browser.find_element(By.XPATH,"//input[@data-test='username']").send_keys(login)
     browser.find_element(By.XPATH,"//input[@data-test='password']").send_keys(password)
     browser.find_element(By.XPATH,"//input[@data-test='login-button']").click()
-
-    assert(browser.current_url == 'https://www.saucedemo.com/inventory.html')
+    yield
     browser.delete_all_cookies()
 
-
-def test_purchase():
-    browser.get('http://www.saucedemo.com/')
-    browser.find_element(By.XPATH,"//input[@data-test='username']").send_keys(login)
-    browser.find_element(By.XPATH,"//input[@data-test='password']").send_keys(password)
-    browser.find_element(By.XPATH,"//input[@data-test='login-button']").click()
-
+@pytest.fixture
+def add_item(browser_auth):
     browser.find_element(By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-backpack']").click()
     browser.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']").click()
+    yield
+
+def test_auth(browser_auth):
+    assert(browser.current_url == 'https://www.saucedemo.com/inventory.html')
+
+def test_purchase(add_item):
     assert('Sauce Labs Backpack' in [ i.text for i in browser.find_elements(By.XPATH, "//div[@data-test='inventory-item-name']")])
     browser.get('https://www.saucedemo.com/inventory.html')
     browser.find_element(By.XPATH, "//button[@data-test='remove-sauce-labs-backpack']").click()
-    browser.delete_all_cookies()
 
-def test_delete():
-    browser.get('http://www.saucedemo.com/')
-    browser.find_element(By.XPATH,"//input[@data-test='username']").send_keys(login)
-    browser.find_element(By.XPATH,"//input[@data-test='password']").send_keys(password)
-    browser.find_element(By.XPATH,"//input[@data-test='login-button']").click()
-
-    browser.find_element(By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-backpack']").click()
-    browser.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']").click()
+def test_delete(add_item):
     browser.get('https://www.saucedemo.com/inventory.html')
     browser.find_element(By.XPATH, "//button[@data-test='remove-sauce-labs-backpack']").click()
     browser.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']").click()
     assert( [] == browser.find_elements(By.XPATH, "//div[@data-test='inventory-item']"))
 
 def test_timeout():
-
     browser.set_page_load_timeout(5)
 
     browser.get('http://www.saucedemo.com/')
@@ -61,15 +54,7 @@ def test_timeout():
     browser.find_element(By.XPATH,"//input[@data-test='password']").send_keys(password)
     browser.find_element(By.XPATH,"//input[@data-test='login-button']").click()
 
-
-def test_order():
-    browser.get('http://www.saucedemo.com/')
-    browser.find_element(By.XPATH,"//input[@data-test='username']").send_keys(login)
-    browser.find_element(By.XPATH,"//input[@data-test='password']").send_keys(password)
-    browser.find_element(By.XPATH,"//input[@data-test='login-button']").click()
-    browser.find_element(By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-backpack']").click()
-    browser.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']").click()
-
+def test_order(add_item):
     browser.find_element(By.XPATH, "//button[@data-test='checkout']").click()
     browser.find_element(By.XPATH, "//input[@data-test='firstName']").send_keys('qwerty')
     browser.find_element(By.XPATH, "//input[@data-test='lastName']").send_keys('qwerty')
@@ -79,4 +64,3 @@ def test_order():
 
     browser.find_element(By.XPATH, "//button[@data-test='finish']").click()
     assert("Thank you for your order!" == browser.find_element(By.XPATH, "//h2[@data-test='complete-header']").text)
-    browser.delete_all_cookies()
